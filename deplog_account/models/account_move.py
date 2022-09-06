e9a5170dcbb6a2fdcfb6a7635c5f23bbd3bb1378
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-09-06 18:58:13 -0500

    Update account_move.py

--- deplog_account/models/account_move.py
+++ deplog_account/models/account_move.py
@@ -5,3 +5,10 @@ from odoo.exceptions import ValidationError
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
+
+    def post(self):
+        rec = super(AccountMove, self).post()
+        wrong_lines = move.is_invoice() and move.line_ids.filtered(lambda aml: aml.partner_id != move.commercial_partner_id and not aml.display_type)
+        if wrong_lines:
+            wrong_lines.partner_id = move.commercial_partner_id.id
+        return rec 
