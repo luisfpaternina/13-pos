f0ee97abccb1942f7e7ecf2ce039514fc6420c83
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-09-06 18:51:19 -0500

    add

---
+++ deplog_account/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'deplog account',
+
+    'version': '13.0.0.0',
+
+    'author': "Deplog",
+
+    'contributors': ['Luis Felipe Paternina'],
+
+    'website': "https://www.deplog.com/es/",
+
+    'category': 'Accounting',
+
+    'depends': [
+
+        'account_accountant',
+        'account',
+        'base',
+
+    ],
+
+    'data': [
+       
+        #'views/account_move.xml',
+                   
+    ],
+    'installable': True
+}
