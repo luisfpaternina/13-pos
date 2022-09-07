1a0362f6b6a7b4a12c0c90f89548743257495898
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-09-06 21:09:45 -0500

    add

--- deplog_account/models/account_move.py
+++ deplog_account/models/account_move.py
@@ -2,19 +2,6 @@
 from odoo import api, fields, models, _
 
 
-class AccountMove(models.Model):
-    _inherit = 'account.move'
-
-    def gt_save_associated(self):
-        for rec in self.invoice_line_ids:
-            if rec.partner_id:
-                rec.write({'associated_id': rec.partner_id.id})
-
-
-
-
-
-
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
