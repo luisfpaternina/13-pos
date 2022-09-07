3b5b6ba092058ca3f68966c878d4e755eb8b1213
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-09-06 19:02:01 -0500

    Update account_move.py

--- deplog_account/models/account_move.py
+++ deplog_account/models/account_move.py
@@ -8,7 +8,10 @@ class AccountMove(models.Model):
 
     def post(self):
         rec = super(AccountMove, self).post()
-        wrong_lines = move.is_invoice() and move.line_ids.filtered(lambda aml: aml.partner_id != move.commercial_partner_id and not aml.display_type)
-        if wrong_lines:
-            wrong_lines.partner_id = move.commercial_partner_id.id
-        return rec 
+        for move in self.sorted(lambda m: (m.date, m.ref or '', m.id)):
+            if move.auto_post and move.date > fields.Date.today():
+                raise UserError(_("This move is configured to be auto-posted on {}".format(move.date.strftime(get_lang(self.env).date_format))))
+            wrong_lines = move.is_invoice() and move.line_ids.filtered(lambda aml: aml.partner_id != move.commercial_partner_id and not aml.display_type)
+            if wrong_lines:
+                wrong_lines.partner_id = move.commercial_partner_id.id
+        return rec
