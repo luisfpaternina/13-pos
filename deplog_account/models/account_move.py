2d1e57f737db7665fbbe472d046a474ee6c1fcf5
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-09-06 20:33:52 -0500

    Update account_move.py

--- deplog_account/models/account_move.py
+++ deplog_account/models/account_move.py
@@ -16,3 +16,8 @@ class AccountMoveLine(models.Model):
     associated_id = fields.Many2one(
         'res.partner',
         string="Associated")
+
+    def save_associated(self):
+        for rec in self:
+            if rec.partner_id:
+                rec.write({'associated_id': rec.partner_id.id})
